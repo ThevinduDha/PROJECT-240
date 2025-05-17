46f0ef45ecbd0041d875f475763b92d05b553e14
IT24610817 <IT24610817@my.sliit.lk>
2025-05-17 15:21:20 +0530

    Updated

---
+++ EventBooking/src/main/java/com/eventBooking/models/admin/Admin.java
@@ -0,0 +1,12 @@
+package com.eventBooking.models.admin;
+
+import com.eventBooking.models.user.User;
+
+public class Admin extends User {
+
+    public Admin(String username, String password, String email) {
+        super(username, password, email);
+    }
+
+
+}
