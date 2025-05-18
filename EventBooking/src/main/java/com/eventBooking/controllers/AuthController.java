66ffa304747fd1ba2fc0e04fc784541a28aba916
Thevindu Dharmadasa <thevinduhansaja81@gmail.com>
2025-05-18 19:37:12 +0530

    Updated✅

--- EventBooking/src/main/java/com/eventBooking/controllers/AuthController.java
+++ EventBooking/src/main/java/com/eventBooking/controllers/AuthController.java
@@ -1,14 +1,16 @@
 package com.eventBooking.controllers;
 
-import com.eventBooking.models.users.User;
+import com.eventBooking.models.user.User;
 import com.eventBooking.services.AdminService;
 import com.eventBooking.services.BookingService;
 import com.eventBooking.services.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 
 import org.springframework.stereotype.Controller;
 
@@ -21,28 +23,32 @@ public class AuthController {
 
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "common/index";
     }
 
     @GetMapping("/dashboard")
-    public String dashboard(Model model, HttpSession session) {
+    public String dashboard(Model model, HttpSession session, HttpServletResponse response) {
         String username = session.getAttribute("username").toString();
-
         model.addAttribute("bookings", bookingService.getBookingsByUser(username));
         logger.info("User booking fetched in /dashboard: {}", bookingService.getBookingsByUser(username));
-        return "dashboard";
+
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Expires", "0");
+
+        return "common/dashboard";
     }
 
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("error", false);
-        return "login";
+        return "user/login";
     }
 
     @GetMapping("/admin/login")
     public String adminLogin(Model model) {
         model.addAttribute("error", false);
-        return "admin-login";
+        return "admin/admin-login";
     }
 
     @PostMapping("/login")
@@ -54,9 +60,9 @@ public class AuthController {
         if(adminService.validateAdminLogin(username, password)) {
             session.setAttribute("username", username);
             session.setAttribute("password", password);
-            session.setAttribute("role", "com/eventBooking/models/admin");
+            session.setAttribute("role", "admin");
             model.addAttribute("error", false);
-            return "admin-dashboard";
+            return "admin/admin-dashboard";
         }
         // Then check if it's a regular user login
         else if(userService.validateLogin(username, password)) {
@@ -66,26 +72,24 @@ public class AuthController {
 
             // Check if the user has admin role in users.txt
             User user = userService.getUserByUsername(username);
-            if(user != null && user.getEmail().equals("com/eventBooking/models/admin")) {
-                session.setAttribute("role", "com/eventBooking/models/admin");
-                return "admin-dashboard";
+            if(user != null && user.getEmail().equals("admin")) {
+                session.setAttribute("role", "admin");
+                return "admin/admin-dashboard";
             }
             else {
-                session.setAttribute("role", "com/eventBooking/models/user");
+                session.setAttribute("role", "user");
                 return "redirect:/dashboard";
             }
         }
-        else {
-            model.addAttribute("message", "Invalid username or password");
-            model.addAttribute("error", true);
-            return "login";
-        }
+        model.addAttribute("message", "Invalid username or password");
+        model.addAttribute("error", true);
+        return "user/login";
     }
 
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("error", false);
-        return "register";
+        return "user/register";
     }
 
     @PostMapping("/register")
@@ -93,24 +97,24 @@ public class AuthController {
                            @RequestParam String password,
                            @RequestParam String confirmPassword,
                            HttpSession session, Model model) {
-        User newUser = new User(username, password, "com/eventBooking/models/user");
+        User newUser = new User(username, password, "user");
         if (!password.equals(confirmPassword)) {
             model.addAttribute("message", "Passwords do not match");
-            return "register";
+            return "user/register";
         }
         if (userService.registerUser(newUser)){
-            return "dashboard";
+            return "common/dashboard";
         }
         else {
             model.addAttribute("message", "User already exists. Login instead");
             model.addAttribute("error", true);
-            return "login";
+            return "user/login";
         }
     }
 
     @GetMapping("/about")
     public String about() {
-        return "about";
+        return "common/about";
     }
 
 
